4d93a379419269889957a35581c186ff88299d42
Shubham Sharma <sh1nine96@gmail.com>
2022-12-12 13:22:07 +0530

    VideoCard

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoChannelTitle,
+} from "../utils/constants";
 
-const VideoCard = () => {
-  return <div>VideoCard</div>;
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  // console.log(videoId, snippet);
+  return (
+    <Card>
+      <Link>
+        <CardMedia image={snippet?.thumbnails?.high?.url} />
+      </Link>
+    </Card>
+  );
 };
 
 export default VideoCard;
